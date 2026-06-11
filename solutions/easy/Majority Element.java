ffc9c9d6d03e21b695a5447655fa910291681a80
Rajnandini Ghosh <rajnandinighosh09@gmail.com>
2026-06-12 01:43:55 +0530

    Add solution for Majority Element

---
+++ solutions/easy/Majority Element.java
@@ -0,0 +1,22 @@
+// Title: Majority Element
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/majority-element/
+
+            if (el == val){
+        }
+                freq++;
+            }
+        for (int el: nums){
+        if (freq>n/2){
+            return val;
+        } 
+    }
+
+    return -1;
+}}
+            int freq = 0;
+        for (int val: nums){
+        int n = nums.length;
+    public int majorityElement(int[] nums) {
+class Solution {
