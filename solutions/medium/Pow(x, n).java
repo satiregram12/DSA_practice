556dc18eac97286a08d2226dcab0bf58c8f35d8d
Rajnandini Ghosh <rajnandinighosh09@gmail.com>
2026-06-12 02:44:15 +0530

    Add solution for Pow(x, n)

---
+++ solutions/medium/Pow(x, n).java
@@ -0,0 +1,11 @@
+// Title: Pow(x, n)
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/pow(x,-n)/
+
+class Solution {
+    public double myPow(double x, int n) {
+        double p = Math.pow(x,n);
+    }
+        return p; 
+}
