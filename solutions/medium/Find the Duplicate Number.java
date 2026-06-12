bda70b3bb602db6d4f482fa0a32dd0f6713140cc
Rajnandini Ghosh <rajnandinighosh09@gmail.com>
2026-06-13 01:45:35 +0530

    Add solution for Find the Duplicate Number

---
+++ solutions/medium/Find the Duplicate Number.java
@@ -0,0 +1,23 @@
+// Title: Find the Duplicate Number
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/find-the-duplicate-number/
+
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        slow = nums[0];
+
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+}
