adc465ffbcf2fa409a0e027047da3eb1bf1a033f
Rajnandini Ghosh <rajnandinighosh09@gmail.com>
2026-06-13 02:53:10 +0530

    Add solution for Next Permutation

---
+++ solutions/medium/Next Permutation.java
@@ -0,0 +1,26 @@
+// Title: Next Permutation
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/next-permutation/
+
+            swap(nums,i,j);
+        }
+
+
+            }
+                j--;
+            while(nums[j] <= nums[i]){
+
+
+            int j = nums.length - 1;
+        if(i>=0){
+
+        }
+        while(i>=0 && nums[i] >= nums[i+1]){
+            i--;
+
+
+        int i = nums.length - 2;
+    public void nextPermutation(int[] nums) {
+
+class Solution {
