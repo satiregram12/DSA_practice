4e43f3353619d88d6ec89dfb87f36ec779e7c00d
Rajnandini Ghosh <rajnandinighosh09@gmail.com>
2026-06-13 02:55:07 +0530

    Add solution for 4Sum

---
+++ solutions/medium/4Sum.java
@@ -0,0 +1,26 @@
+// Title: 4Sum
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/4sum/
+
+
+            for(int j=i+1;j<n-2;j++){
+
+
+        Arrays.sort(nums);
+
+        int n = nums.length;
+
+        for(int i=0;i<n-3;i++){
+
+            if(i>0 && nums[i]==nums[i-1])
+                continue;
+    public List<List<Integer>> fourSum(int[] nums,
+                                       int target) {
+
+        List<List<Integer>> ans =
+                new ArrayList<>();
+import java.util.*;
+
+class Solution {
+
