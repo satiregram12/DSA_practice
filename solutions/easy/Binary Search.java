59a5f4dc91ab36cfed949685bf12a1d81bd3fab9
Rajnandini Ghosh <rajnandinighosh09@gmail.com>
2026-08-15 22:40:53 +0530

    Add solution for Binary Search

---
+++ solutions/easy/Binary Search.java
@@ -0,0 +1,19 @@
+// Title: Binary Search
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/binary-search/
+
+        else if (target>nums[mid]) 
+        else{
+            end = mid-1;
+            start = mid+1;
+        {
+            }
+            }
+        }
+        mid = start + (end-start)/2;
+       }
+       return -1;
+    }
+    
+}
