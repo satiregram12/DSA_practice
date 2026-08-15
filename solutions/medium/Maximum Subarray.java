16e737c3f0c7eaaedda00eee7b013a0d840d5ceb
Rajnandini Ghosh <rajnandinighosh09@gmail.com>
2026-08-15 11:25:24 +0530

    Add solution for Maximum Subarray

--- solutions/medium/Maximum Subarray.java
+++ solutions/medium/Maximum Subarray.java
@@ -5,18 +5,12 @@
 
 class Solution {
     public int maxSubArray(int[] nums) {
-
-        int currentSum = nums[0];
-        int maxSum = nums[0];
-
-        for(int i = 1; i < nums.length; i++) {
-
-            currentSum = Math.max(nums[i],
-                                  currentSum + nums[i]);
-
-            maxSum = Math.max(maxSum, currentSum);
-        }
-
-        return maxSum;
+        int maxi = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = 0;i<nums.length;i++){
+            sum = sum + nums[i];
+            maxi = Math.max(maxi,sum);
+        }return maxi;
     }
+            if ( sum<0) sum = 0;
 }
