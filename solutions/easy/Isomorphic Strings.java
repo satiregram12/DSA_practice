e3534cc56723fdf5da450ba67908d88e7521f069
Rajnandini Ghosh <rajnandinighosh09@gmail.com>
2026-08-19 16:15:27 +0530

    Add solution for Isomorphic Strings

---
+++ solutions/easy/Isomorphic Strings.java
@@ -0,0 +1,26 @@
+// Title: Isomorphic Strings
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/isomorphic-strings/
+
+            }
+            else if(hm.containsValue(t.charAt(i)))
+                return false;
+            {
+                if(hm.get(s.charAt(i)) != t.charAt(i)) 
+                    return false;
+            if(hm.containsKey(s.charAt(i)))
+        {
+        for(int i=0 ; i<s.length() ; i++)
+
+        if(s.length()!=t.length())  return false;
+        HashMap <Character,Character> hm = new HashMap<>();
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+            
+            hm.put(s.charAt(i),t.charAt(i));
+
+        }
+        return true;
+    }
+}
