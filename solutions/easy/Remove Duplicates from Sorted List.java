084d36f23e8de788f2d08b4bf2bf25884b95a249
Rajnandini Ghosh <rajnandinighosh09@gmail.com>
2026-08-21 19:08:15 +0530

    Add solution for Remove Duplicates from Sorted List

---
+++ solutions/easy/Remove Duplicates from Sorted List.java
@@ -0,0 +1,26 @@
+// Title: Remove Duplicates from Sorted List
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+
+        }
+        ListNode prev = head;
+        ListNode curr = head.next;
+
+        while (curr != null){
+            if (prev.val != curr.val){
+                prev = prev.next;
+            return head;
+        if (head.next == null){
+        }
+            return head;
+        if (head == null){
+    public ListNode deleteDuplicates(ListNode head) {
+class Solution {
+                curr = curr.next;
+            }
+            else{
+                prev.next = curr.next;
+                curr = curr.next;
+            }
+ */
