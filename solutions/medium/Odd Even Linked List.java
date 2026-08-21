13f5bb203a6ec63963f941d39a19b4b863e6ddd5
Rajnandini Ghosh <rajnandinighosh09@gmail.com>
2026-08-21 23:57:04 +0530

    Add solution for Odd Even Linked List

--- solutions/medium/Odd Even Linked List.java
+++ solutions/medium/Odd Even Linked List.java
@@ -3,23 +3,4 @@
             // Language: Java
             // Link: https://leetcode.com/problems/odd-even-linked-list/
 
-        ListNode oddTail = head;
-
-        ListNode evenHead = head.next;
-        ListNode evenTail = head.next;
-
-        while (evenTail != null && evenTail.next != null) {
-
-            oddTail.next = evenTail.next;
-            oddTail = evenTail.next;
-
-        ListNode oddHead = head;
-            evenTail.next = oddTail.next;
-            evenTail = oddTail.next;
-        }
-
-        oddTail.next = evenHead;
-
-        return oddHead;
-    }
-}
+/
