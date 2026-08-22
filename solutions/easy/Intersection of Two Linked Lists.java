d6e26be9a1e036729beacb02e3c857db51739744
Rajnandini Ghosh <rajnandinighosh09@gmail.com>
2026-08-22 12:57:58 +0530

    Add solution for Intersection of Two Linked Lists

---
+++ solutions/easy/Intersection of Two Linked Lists.java
@@ -0,0 +1,26 @@
+// Title: Intersection of Two Linked Lists
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/intersection-of-two-linked-lists/
+
+        }
+        while (aextraLen-- > 0){
+            headA = headA.next;
+        }
+       }
+       while (headA != null && headB != null){
+        if (headA == headB){
+            a = a.next;
+            aextraLen ++;
+        while (a!= null){
+        int aextraLen = 0;
+       else{
+       }
+        }
+            headB = headB.next;
+        while (bextraLen-- > 0){
+        }
+            b = b.next;
+            bextraLen ++;
+        while (b!= null){
+        int bextraLen = 0;
